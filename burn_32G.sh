bba29a0fdf2a9372fce54ed5e0642df16148b66a
ziqiang.peng <347126521@qq.com>
2022-12-22 20:08:04 +0800

    enable led control
    Signed-off-by: ziqiang.peng <347126521@qq.com>

---
+++ burn_32G.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "get the bl1.bin (8K) from the u-boot.bin"
+sudo dd if=./u-boot.bin of=bl1.bin bs=512 count=16
+echo "burn the bl1.bin into the sdcard"
+sudo dd if=./bl1.bin of=/dev/sdc seek=60439534 bs=512 count=16
+sync
+
